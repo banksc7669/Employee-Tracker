b878638a5600c4a162b76ee653ab2134dd344b44
CBanks <banksc7669@yahoo.com>
2020-11-24 20:26:01 -0500

    update to seed.sql

--- tracker.js
+++ tracker.js
@@ -1,34 +1,65 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
 
 // create the connection information for the sql database
-var connection = mysql.createConnection({
-  host: "localhost",
+const connection = mysql.createConnection({
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "root1234",
-  database: "employee_tracker_DB"
+    // Your password
+    password: "root1234",
+    database: "employee_tracker_DB"
 });
 
 // connect to the mysql server and sql database
-connection.connect(function(err) {
-  if (err) throw err;
-  // run the start function after the connection is made to prompt the user
-  start();
+connection.connect(function (err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
 });
 
 // function which prompts the user for what action they should take
-function start() {
-  inquirer
-    .prompt({
-      name: "postOrBid",
-      type: "list",
-      message: "Welcome to the employee tracker management system. What would you like do or view?",
-      choices: ["POST", "BID", "EXIT"]
-    })
+start() => {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "Welcome to the employee tracker management system. What would you like view, add, or update?",
+            choices: [
+                "View employees",
+                "View departments",
+                "View roles",
+                "Add employee",
+                "Add department",
+                "Add role",
+                "EXIT"
+            ]
+        })
+        .then((answer) => {
+            // based on their answer, either call view or Add functions
+            switch (answer.action) {
+                case "View employees":
+                    viewEmployees();
+                    break;
+                case "View departments":
+                    viewDepartments();
+                    break;
+                case "View roles":
+                    viewRolls();
+                    break;
+                case "Add employees":
+                    addEmployees();
+                    break;
+                case "Add departments":
+                    addDepartments();
+                    break;
+                case "Add roles":
+                    addRolls();
+                    break;
+            }
+        });
